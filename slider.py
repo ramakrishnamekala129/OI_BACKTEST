4d785da0b1026e29a51d6449f614859939ec98c3
ramakrishnamekala129 <ramakrishnamekala129@gmail.com>
2023-04-09 18:05:08 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -3,7 +3,10 @@ from datetime import time,timedelta
 import pandas as pd
 import plotly.express as px 
 import json
+import math
 import datetime
+import numpy as np
+from numpy import nan
 import os
 st.set_page_config(layout="wide")
 dates=[]
@@ -17,6 +20,8 @@ for i in os.listdir():
 o=st.selectbox('Select Date :',dates)
 st.write('Display Date is:', datetime.date(2023, 3, 9))
 # Opening JSON file
+m=st.sidebar.text_input('Option Range',value=10)
+
 
 for i in filesnames:
     if o in i:
@@ -28,79 +33,88 @@ for i in filesnames:
 appointment = st.slider(    "Select Market time:",    min_value=time(9, 15),max_value=time(15,30),    value=time(9, 16),step=timedelta(minutes=1))
 st.write("Current Market Time:", appointment)
 
-#st.write(data)
-#st.write(data[str(appointment)])
 col1, col2, col3 ,col4= st.columns(4)
 col1.metric("ATM", data[str(appointment)]['ATM'], "")
 col2.metric("Spot Price", data[str(appointment)]['Spot'], "")
 col3.metric("MarketProfile", data[str(appointment)]['Market_Profile_poc'], "")
 col4.metric("VolumeProfile", data[str(appointment)]['Volume_Profile_poc'], "")
 
+data1=data[str(appointment)]
+
+eodselect=pd.DataFrame.from_dict(data1['10Strike'])
+strikes=[]
+for i in range(0,6):
+    strikes.append(data1['ATM']+(i*100))
+    strikes.append(data1['ATM']-(i*100))
+eodselect=eodselect[eodselect['#'].isin(strikes)]
+eodselect['PCR']=round((eodselect['PE_TOI']/eodselect['CE_TOI']),2)
+eodselect['CPR']=round((eodselect['CE_TOI']/eodselect['PE_TOI']),2)
+eodselect['CE_PCT']=round((eodselect['CE_PCT']*100),2)
+eodselect['PE_PCT']=round((eodselect['PE_PCT']*100),2)
+eodselect['CE_PCT']=eodselect['CE_PCT'].apply(str)+'%'
+eodselect['PE_PCT']=eodselect['PE_PCT'].apply(str)+'%'
 
 
-col1, col2, col3 ,col4= st.columns(4)
-col1.metric("CE_COI_5", pd.DataFrame(data[str(appointment)]['5Strike'])['CE_COI'].apply(int).sum(), "")#-pd.DataFrame(data['09:18:00']['5Strike'])['CE_COI'].apply(int).iloc[0], "")
-col2.metric("PE_COI_5", pd.DataFrame(data[str(appointment)]['5Strike'])['PE_COI'].apply(int).sum(), "")#-pd.DataFrame(data['09:18:00']['5Strike'])['PE_COI'].apply(int).iloc[0], "")
-col3.metric("CE_COI_10", pd.DataFrame(data[str(appointment)]['10Strike'])['CE_COI'].apply(int).sum(), "")#-pd.DataFrame(data[str(appointment)]['10Strike'])['CE_COI'].apply(int).iloc[0], "")
-col4.metric("PE_COI_10", pd.DataFrame(data[str(appointment)]['10Strike'])['PE_COI'].apply(int).sum(), "")#-pd.DataFrame(data[str(appointment)]['10Strike'])['PE_COI'].apply(int).iloc[0], "")
+df=(eodselect[['#','CE_TOI','PE_TOI','CPR','PCR','CE_COI','PE_COI','CE_INOI','PE_INOI','CE_PCT','PE_PCT']].iloc[::-1])
+R=[]
+S=[]
+for i in range(0,6):
+    if not (df[(df['CPR']>= 1.5) & (df['#']==(data1['ATM']+(i*100)))]).empty:
+        print(str(data1['ATM']+(i*100)))
+        R.append(data1['ATM']+(i*100))
+    if not (df[(df['PCR']>= 1.5) & (df['#']==(data1['ATM']-(i*100)))]).empty:
+        print(str(data1['ATM']-(i*100)))
+        S.append(data1['ATM']-(i*100))
+        
+R=[]
+S=[]
+for i in range(0,6):
+    if not (df[(df['CPR']>= 1.5) & (df['#']==(data1['ATM']+(i*100)))]).empty:
+        print(str(data1['ATM']+(i*100)))
+        R.append(data1['ATM']+(i*100))
+    if not (df[(df['PCR']>= 1.5) & (df['#']==(data1['ATM']-(i*100)))]).empty:
+        print(str(data1['ATM']-(i*100)))
+        S.append(data1['ATM']-(i*100))
+        
+R=R[:3]
+S=S[:3]
+df1=pd.DataFrame([R,S],columns=['R/S_1','R/S_2','R/S_3'])  
+df1.index=['R','S']
+st.dataframe(df1)
+vp=data[str(appointment)]['VolumeProfile']
+mp=data[str(appointment)]['MarketProfile']
+df2=pd.DataFrame([[int(vp['poc']),int(vp['low']),int(vp['high']),int(vp['val']),int(vp['vah']),int(vp['or_low']),int(vp['or_high']),int(vp['ib_low']),int(vp['ib_high']),int(vp['bt'])],
+[int(mp['poc']),int(mp['low']),int(mp['high']),int(mp['val']),int(mp['vah']),int(mp['or_low']),int(mp['or_high']),int(mp['ib_low']),int(mp['ib_high']),int(mp['bt'])]],columns=['POC','Low','High','VAL','VAH','Or_low','Or_High','IB_Low','IB_High','BT'])
+df2.index=['VolumeProfile','MarketProfile']
+st.dataframe(df2)
 
+CE_COI_5=pd.DataFrame(data[str(appointment)]['5Strike'])['CE_COI'].apply(int).sum()
+PE_COI_5=pd.DataFrame(data[str(appointment)]['5Strike'])['PE_COI'].apply(int).sum()
+CE_COI_10=pd.DataFrame(data[str(appointment)]['10Strike'])['CE_COI'].apply(int).sum()
+PE_COI_10=pd.DataFrame(data[str(appointment)]['10Strike'])['PE_COI'].apply(int).sum()
 
+CE_TOI_5=pd.DataFrame(data[str(appointment)]['5Strike'])['CE_TOI'].apply(int).sum()
+PE_TOI_5=pd.DataFrame(data[str(appointment)]['5Strike'])['PE_TOI'].apply(int).sum()
+CE_TOI_10=pd.DataFrame(data[str(appointment)]['10Strike'])['CE_TOI'].apply(int).sum()
+PE_TOI_10=pd.DataFrame(data[str(appointment)]['10Strike'])['PE_TOI'].apply(int).sum()
 
-col1, col2, col3 ,col4= st.columns(4)
-
-
-col1.metric("CE_TOI_5", pd.DataFrame(data[str(appointment)]['5Strike'])['CE_TOI'].apply(int).sum(), "")
-col2.metric("PE_TOI_5", pd.DataFrame(data[str(appointment)]['5Strike'])['PE_TOI'].apply(int).sum(), "")
-
-col3.metric("CE_TOI_10", pd.DataFrame(data[str(appointment)]['10Strike'])['CE_TOI'].apply(int).sum(), "")
-col4.metric("PE_TOI_10", pd.DataFrame(data[str(appointment)]['10Strike'])['PE_TOI'].apply(int).sum(), "")
-
-col1, col2, col3 ,col4,col5,col6= st.columns(6)
-
-
-col1.metric("EOD_R1", data['EOD_RS']['EOD_R1'], "")
-col2.metric("EOD_S1", data['EOD_RS']['EOD_S1'], "")
-
-col3.metric("5_R1", data[str(appointment)]['5RS']['5_R1'], "")
-col4.metric("5_S1", data[str(appointment)]['5RS']['5_S1'], "")
-
-col5.metric("10_R1", data[str(appointment)]['10RS']['10_R1'], "")
-col6.metric("10_S1", data[str(appointment)]['10RS']['10_S1'], "")
-
-
-
-col1, col2, col3 ,col4,col5,col6= st.columns(6)
-
-
-col1.metric("EOD_R2", data['EOD_RS']['EOD_R2'], "")
-col2.metric("EOD_S2", data['EOD_RS']['EOD_S2'], "")
-
-col3.metric("5_R2", data[str(appointment)]['5RS']['5_R2'], "")
-col4.metric("5_S2", data[str(appointment)]['5RS']['5_S2'], "")
-
-col5.metric("10_R2", data[str(appointment)]['10RS']['10_R2'], "")
-col6.metric("10_S2", data[str(appointment)]['10RS']['10_S2'], "")
-
-
-
-col1, col2, col3 ,col4,col5,col6= st.columns(6)
-
-
-col1.metric("EOD_R3", data['EOD_RS']['EOD_R3'], "")
-col2.metric("EOD_S3", data['EOD_RS']['EOD_S3'], "")
-
-col3.metric("5_R3", data[str(appointment)]['5RS']['5_R3'], "")
-col4.metric("5_S3", data[str(appointment)]['5RS']['5_S3'], "")
-
-col5.metric("10_R3", data[str(appointment)]['10RS']['10_R3'], "")
-col6.metric("10_S3", data[str(appointment)]['10RS']['10_S3'], "")
-
-
-
+df3=pd.DataFrame([[CE_COI_5,PE_COI_5,CE_TOI_5,PE_TOI_5],[CE_COI_10,PE_COI_10,CE_TOI_10,PE_TOI_10]],
+             columns=['CE_COI','PE_COI','CE_TOI','PE_TOI'])
+df3.index=['5_Strike','10_Strike']
+df3['COI_Diff']=df3['PE_COI']-df3['CE_COI']
+df3['COI_Decision']=np.where((df3['PE_COI']/df3['CE_COI']) >1.5,'BUY' ,np.where((df3['PE_COI']/df3['CE_COI'])>1,'Strong Buy',np.where((df3['PE_COI']/df3['CE_COI'])<0.5,'Strong SELL','Sell')))
+df3['TOI_Decision']=np.where((df3['PE_TOI']/df3['CE_TOI']) >1.5,'BUY' ,np.where((df3['PE_TOI']/df3['CE_TOI'])>1,'Strong Buy',np.where((df3['PE_TOI']/df3['CE_TOI'])<0.5,'Strong SELL','Sell')))
+df3['TOI_Diff']=df3['PE_TOI']-df3['CE_TOI']
+#df3['COI_Position']=df3['PE_']
+df3=df3[['CE_COI','PE_COI','COI_Diff','COI_Decision','CE_TOI','PE_TOI','TOI_Diff','TOI_Decision']]
+st.dataframe(df3)
+#st.write(data)
+#st.write(data[str(appointment)])
+st.dataframe(df)
 st.write('Volume Profile')
-st.json(data[str(appointment)]['VolumeProfile'])
+st.json(vp)
 st.write('Market Profile')
-st.json(data[str(appointment)]['MarketProfile'])
+st.json(mp)
 
 st.write('EOD')
 
